f9db6afe55637c4b38dfa4b30a2ed0f2c7d1641b
YuYoHan <zxzz8014@naver.com>
2023-01-11 21:41:39 +0900

    [Study] Code02

---
+++ src/session01/Code02.java
@@ -0,0 +1,13 @@
+package session01;
+
+public class Code02 {
+    static int num;
+    public static void main(String[] args) {
+        int otherNum = 5;
+        num = 2;
+
+        System.out.println(num + otherNum);
+        System.out.println("num : " + num);
+        System.out.println("otherNum : " + otherNum);
+    }
+}
