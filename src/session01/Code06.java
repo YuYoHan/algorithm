f969c21663a82f444d96c3bc3373e02ce5146ef5
YuYoHan <zxzz8014@naver.com>
2023-01-12 15:37:21 +0900

    [Study] Code06

---
+++ src/session01/Code06.java
@@ -0,0 +1,23 @@
+package session01;
+
+public class Code06 {
+    public static void main(String[] args) {
+        int[] grades;
+        int n = 5;
+
+        grades = new int[n];
+        grades[0] = 100;
+        grades[1] = 94;
+        grades[2] = 92;
+        grades[3] = 89;
+        grades[4] = 34;
+
+
+
+        for (int i = 0; i < grades.length; i++) {
+            System.out.println(grades[i]);
+        }
+
+
+    }
+}
