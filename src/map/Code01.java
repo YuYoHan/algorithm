ba6c869c601b227641dcd186b4a0faa6bf396e66
YuYoHan <zxzz8014@naver.com>
2023-01-24 13:24:07 +0900

    [Study] map

---
+++ src/map/Code01.java
@@ -0,0 +1,31 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Code01 {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<String, String>();
+
+        // key는 중복을 허용하지 않고 value는 중복을 허용한다.
+        map.put("김씨", "어부");
+        map.put("이씨", "어부");
+        map.put("박씨", "어부");
+
+        for(String key : map.keySet()) {
+            System.out.println(key + " : " + map.get(key));
+        }
+
+        // 순서가 유지되지 않는것을 정확히 보기 위해서 value에 숫자를 넣어서 확인
+        Map<String , Integer> map2 = new HashMap<>();
+
+        map2.put("김", 1);
+        map2.put("이", 2);
+        map2.put("박", 3);
+        map2.put("서", 4);
+
+        for (String key2 : map2.keySet()) {
+            System.out.println(key2 + " : " + map2.get(key2));
+        }
+    }
+}
