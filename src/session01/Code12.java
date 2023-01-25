1516fb975c3b7032637f7f01aba7c006c8f36837
YuYoHan <zxzz8014@naver.com>
2023-01-25 13:16:15 +0900

    [Study] Code12

--- src/session01/Code12.java
+++ src/session01/Code12.java
@@ -17,5 +17,20 @@ public class Code12 {
         }
         sc.close();
 
+        for (int i = n-1; i >0; i--) {
+            // data[0] ... data[i]
+            for (int j = 0; j < i; j++) {
+                if(data[j] > data[j +1]) {
+                    // swap data[j] and data[j+1]
+                    int tmp = data[j];
+                    data[j] = data[j+1];
+                    data[j+1] = tmp;
+                }
+            }
+        }
+        System.out.println("Sorted data : ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(data[i]);
+        }
     }
 }
---
+++ src/session01/Code13.java
@@ -0,0 +1,8 @@
+package session01;
+
+public class Code13 {
+    public static void main(String[] args) {
+
+
+    }
+}
