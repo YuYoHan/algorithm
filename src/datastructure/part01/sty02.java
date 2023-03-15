7030668b3f0646b398349f2d5087b661a6b3b266
YuYoHan <zxzz8014@naver.com>
2023-03-15 11:53:10 +0900

    [Study] 여러개  max값 구하기

--- src/datastructure/part01/sty02.java
+++ src/datastructure/part01/sty02.java
@@ -1,25 +1,26 @@
 package datastructure.part01;
 
 public class sty02 {
-    static  int max(int a, int b, int c) {
+    static int max(int a, int b, int c) {
         int max = a;
-        if(max < b) {
+        if (max < b) {
             max = b;
         }
-        if(max < c) {
+        if (max < c) {
             max = c;
-            return max;
         }
+        return max;
     }
+
     public static void main(String[] args) {
         /*
-        *   여기서는 여러개의 값에 대해서 최대값을 구하고자 합니다.
-        * */
-        System.out.println("max(3,2,1) = " + max(3,2,1));
-        System.out.println("max(3,2,2) = " + max(3,2,2));
-        System.out.println("max(3,1,2) = " + max(3,1,2));
-        System.out.println("max(3,2,3) = " + max(3,2,3));
-        System.out.println("max(2,1,3) = " + max(2,1,3));
-        
+         *   여기서는 여러개의 값에 대해서 최대값을 구하고자 합니다.
+         * */
+        System.out.println("max(3,2,1) = " + max(3, 2, 1));
+        System.out.println("max(3,2,2) = " + max(3, 2, 2));
+        System.out.println("max(3,1,2) = " + max(3, 1, 2));
+        System.out.println("max(3,2,3) = " + max(3, 2, 3));
+        System.out.println("max(2,1,3) = " + max(2, 1, 3));
+
     }
 }
