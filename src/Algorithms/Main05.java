d33280aea902fc5dd3317fdadc913db1ca993535
YuYoHan <zxzz8014@naver.com>
2023-11-08 18:06:58 +0900

    [Study] for문

---
+++ src/Algorithms/Main05.java
@@ -0,0 +1,18 @@
+package Algorithms;
+
+import java.util.Scanner;
+
+public class Main05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println(a + b);
+        }
+    }
+}
