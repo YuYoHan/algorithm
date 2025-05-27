77fcb44020c48e7dcb9f801727e74a7b9d085718
YuYoHan <zxzz8014@naver.com>
2025-05-28 01:39:11 +0900

    [Study] 문자열 더하기

---
+++ src/Algorithms/S05.java
@@ -0,0 +1,14 @@
+package Algorithms;
+
+import java.util.Scanner;
+
+public class S05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+
+        System.out.println(id + "??!");
+
+
+    }
+}
